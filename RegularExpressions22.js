26cc23d298f3e90efdb0d070432f8ee816828fe0
Ian Aranha <ianorama@gmail.com>
2018-11-08 20:17:24 -0800

    Restrict possible usernames

---
+++ RegularExpressions22.js
@@ -0,0 +1,56 @@
+// Regular Expressions: Restrict Possible Usernames
+
+// Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+
+// You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+
+// 1) The only numbers in the username have to be at the end. There can be zero or more of them at the end.
+
+// 2) Username letters can be lowercase and uppercase.
+
+// 3) Usernames have to be at least two characters long. A two-letter username can only use alphabet letter characters.
+
+
+// Change the regex userCheck to fit the constraints listed above.
+
+
+
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]{2,}\d*$/i; // Change this line
+let result = userCheck.test(username);
+console.log(result);     //<<<==== true
+
+
+username = "88FjjMM1";
+result = userCheck.test(username);
+console.log(result);     //<<<==== false
+
+
+// EXPLANATIONS:
+
+// The only numbers in the username have to be at the end. 
+// ===>  /\d$/ 
+
+// There can be zero or more of them at the end. 
+// *
+// ===>  /\d*$/
+
+// Username letters can be lowercase and uppercase. 
+// i
+// ===>  /\d*$/i;
+
+
+// Usernames have to be at least two characters long. 
+// {2,} 
+// ===>  /{2,}\d*$/i;
+
+
+// A two-letter username can only use alphabet letter characters. 
+// ^[a-z]
+
+// ===>	/^[a-z]{2,}\d*$/i;
+
+
+
+
